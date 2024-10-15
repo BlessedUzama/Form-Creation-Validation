c3d57acd8dbdc3e7418a0e79921bda8398169a5f
BlessedUzama <uzamablessed@gmail.com>
2024-10-15 22:57:15 +0100

    first attempt task0

---
+++ script.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+
+    const form = document.getElementById('registration-form');
+    const feedbackDiv = document.getElementById('form-feedback');
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault()
+    
+
+        const usernameInput = document.getElementById('username');
+        const username = usernameInput.value.trim();
+        const emailInput = document.getElementById('email');
+        const email = emailInput.value.trim();
+        const passwordInput = document.getElementById('password');
+        const password = passwordInput.value.trim();
+
+        let isValid = true;
+        const messages = [];
+
+        if (username.length < 3) {
+            isValid = false;
+            messages.push('Username must be at least 3 characters long.');
+        }
+
+        if (!email.includes('@') || !email.includes('.')) {
+        isValid = false;
+        messages.push('Email must include both "@" and "." characters.');
+        }
+
+        if (password.length < 8) {
+            isValid = false;
+            messages.push('Password id too short.')
+        }
+
+    
+        feedbackDiv.style.display = 'block';
+        if (isValid == true) {
+            feedbackDiv.textContent = "Registration successful!";
+            feedbackDiv.style.color = '#28a745';
+        }
+        if (isValid == false) {
+            feedbackDiv.innerHTML = messages.join('<br>');
+            feedbackDiv.style.color = '#dc3545'
+
+        }
+    })
+})
